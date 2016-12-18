4170fc5853170467a9608938446925381ba0b63c
Anton Kachurin <kachurinanton@gmail.com>
2016-12-17 23:57:51 -0500

    add locations

--- js/locations.js
+++ js/locations.js
@@ -125,13 +125,252 @@ app.locations = [
       }
     }
   },
-//
-// "reptdistrict": 'D14 - brighton\allston
-// "reptdistrict": 'A15 - charlestown
-// "reptdistrict": 'C6 - south boston
-// "reptdistrict": 'A1 - downtown
-// "reptdistrict": 'B3 - mattapan
-// "reptdistrict": 'C11 - dorchester
-// "reptdistrict": 'B2 - roxbury
-// "reptdistrict": 'D4 - backbay
-]
+  {
+    "reptdistrict": 'D14',
+    "formatted_address" : "Brighton/Allston",
+    "geometry" : {
+       "bounds" : {
+          "northeast" : {
+             "lat" : 42.3601961,
+             "lng" : -71.1225359
+          },
+          "southwest" : {
+             "lat" : 42.33009699999999,
+             "lng" : -71.17479779999999
+          }
+       },
+       "location" : {
+          "lat" : 42.3463503,
+          "lng" : -71.1626756
+       },
+       "location_type" : "APPROXIMATE",
+       "viewport" : {
+          "northeast" : {
+            "lat" : 42.37437389999999,
+            "lng" : -71.110533
+          },
+          "southwest" : {
+             "lat" : 42.33009699999999,
+             "lng" : -71.17479779999999
+          }
+       }
+    }
+  },
+  {
+    "reptdistrict": 'A15',
+    "formatted_address" : "Charlestown",
+    "geometry" : {
+        "bounds" : {
+           "northeast" : {
+              "lat" : 42.3897705,
+              "lng" : -71.04429809999999
+           },
+           "southwest" : {
+              "lat" : 42.3688756,
+              "lng" : -71.07340619999999
+           }
+        },
+        "location" : {
+           "lat" : 42.3782065,
+           "lng" : -71.0602131
+        },
+        "location_type" : "APPROXIMATE",
+        "viewport" : {
+           "northeast" : {
+              "lat" : 42.3897705,
+              "lng" : -71.04429809999999
+           },
+           "southwest" : {
+              "lat" : 42.3688756,
+              "lng" : -71.07340619999999
+           }
+        }
+     }
+  },
+  {
+    "reptdistrict": 'C6',
+    "formatted_address" : "South Boston",
+    "geometry" : {
+      "bounds" : {
+         "northeast" : {
+            "lat" : 42.3551665,
+            "lng" : -71.0091984
+         },
+         "southwest" : {
+            "lat" : 42.32115,
+            "lng" : -71.0652948
+         }
+      },
+      "location" : {
+         "lat" : 42.3381437,
+         "lng" : -71.0475773
+      },
+      "location_type" : "APPROXIMATE",
+      "viewport" : {
+         "northeast" : {
+            "lat" : 42.3551665,
+            "lng" : -71.0091984
+         },
+         "southwest" : {
+            "lat" : 42.32115,
+            "lng" : -71.0652948
+         }
+      }
+    }
+  },
+  {
+    "reptdistrict": 'A1',
+    "formatted_address" : "Downtown",
+    "geometry" : {
+      "bounds" : {
+         "northeast" : {
+            "lat" : 42.36839699999999,
+            "lng" : -71.04779599999999
+         },
+         "southwest" : {
+            "lat" : 42.342797,
+            "lng" : -71.072996
+         }
+      },
+      "location" : {
+         "lat" : 42.3557089,
+         "lng" : -71.05720529999999
+      },
+      "location_type" : "APPROXIMATE",
+      "viewport" : {
+         "northeast" : {
+            "lat" : 42.36839699999999,
+            "lng" : -71.04779599999999
+         },
+         "southwest" : {
+            "lat" : 42.342797,
+            "lng" : -71.072996
+         }
+      }
+    }
+  },
+  {
+    "reptdistrict": 'B3',
+    "formatted_address" : "Mattapan",
+    "geometry" : {
+      "bounds" : {
+         "northeast" : {
+            "lat" : 42.298298,
+            "lng" : -71.06759390000001
+         },
+         "southwest" : {
+            "lat" : 42.2628979,
+            "lng" : -71.10919579999999
+         }
+      },
+      "location" : {
+         "lat" : 42.2771417,
+         "lng" : -71.0913748
+      },
+      "location_type" : "APPROXIMATE",
+      "viewport" : {
+         "northeast" : {
+            "lat" : 42.298298,
+            "lng" : -71.06759390000001
+         },
+         "southwest" : {
+            "lat" : 42.2628979,
+            "lng" : -71.10919579999999
+         }
+      }
+    }
+  },
+  {
+    "reptdistrict": 'C11',
+    "formatted_address" : "Dorchester",
+    "geometry" : {
+      "bounds" : {
+         "northeast" : {
+            "lat" : 42.3358969,
+            "lng" : -71.0325551
+         },
+         "southwest" : {
+            "lat" : 42.267598,
+            "lng" : -71.0800369
+         }
+      },
+      "location" : {
+         "lat" : 42.30163049999999,
+         "lng" : -71.067605
+      },
+      "location_type" : "APPROXIMATE",
+      "viewport" : {
+         "northeast" : {
+            "lat" : 42.3358969,
+            "lng" : -71.0325551
+         },
+         "southwest" : {
+            "lat" : 42.267598,
+            "lng" : -71.0800369
+         }
+      }
+    }
+  },
+  {
+    "reptdistrict": 'B2',
+    "formatted_address" : "Roxbury",
+    "geometry" : {
+      "bounds" : {
+         "northeast" : {
+            "lat" : 42.335885,
+            "lng" : -71.06649519999999
+         },
+         "southwest" : {
+            "lat" : 42.29179790000001,
+            "lng" : -71.10676119999999
+         }
+      },
+      "location" : {
+         "lat" : 42.3151978,
+         "lng" : -71.0913748
+      },
+      "location_type" : "APPROXIMATE",
+      "viewport" : {
+         "northeast" : {
+            "lat" : 42.335885,
+            "lng" : -71.06649519999999
+         },
+         "southwest" : {
+            "lat" : 42.29179790000001,
+            "lng" : -71.10676119999999
+         }
+      }
+    }
+  },
+  {
+    "reptdistrict": 'D4',
+    "formatted_address" : "Back Bay",
+    "geometry" : {
+      "bounds" : {
+         "northeast" : {
+            "lat" : 42.356983,
+            "lng" : -71.07028989999999
+         },
+         "southwest" : {
+            "lat" : 42.341597,
+            "lng" : -71.0932962
+         }
+      },
+      "location" : {
+         "lat" : 42.35026480000001,
+         "lng" : -71.0809757
+      },
+      "location_type" : "APPROXIMATE",
+      "viewport" : {
+         "northeast" : {
+           "lat":42.3596609643262,
+           "lng":-71.03029463691405
+         },
+         "southwest" : {
+           "lat":42.327814621789145,
+           "lng":-71.12299178046874
+         }
+      }
+    }
+  }
+];
